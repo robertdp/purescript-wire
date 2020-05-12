e6b2018f704b464ae16138695fc468616cf5f724
Robert Porter <robert.porter@rakuten.com>
2020-05-12 16:52:06 +0900

    Added `traversable` and `ltraversable` helpers

--- src/Wire/Signal.purs
+++ src/Wire/Signal.purs
@@ -11,7 +11,9 @@ module Wire.Signal
 
 import Prelude
 import Data.Array (deleteBy, snoc)
-import Data.Foldable (traverse_)
+import Data.Bitraversable (class Bitraversable, ltraverse)
+import Data.Either (Either)
+import Data.Foldable (class Foldable, traverse_)
 import Data.Maybe (Maybe(..))
 import Data.Profunctor (class Profunctor, rmap)
 import Effect (Effect)
@@ -74,6 +76,21 @@ filter :: forall i o. (o -> Boolean) -> Signal i o -> Signal i o
 filter predicate (Signal s) = Signal s { subscribe = subscribe }
   where
   subscribe k = s.subscribe \a -> when (predicate a) do k a
+
+traversable :: forall i o f. Foldable f => Signal i o -> Signal (f i) o
+traversable (Signal s) = Signal s { write = traverse_ s.write }
+
+ltraversable :: forall i o a f. Bitraversable f => Signal i o -> Signal (f i a) o
+ltraversable (Signal s) = Signal s { write = void <<< ltraverse s.write }
+
+restrictJust :: forall i o. Signal i o -> Signal (Maybe i) o
+restrictJust = traversable
+
+restrictRight :: forall i o a. Signal i o -> Signal (Either a i) o
+restrictRight = traversable
+
+restrictLeft :: forall i o a. Signal i o -> Signal (Either i a) o
+restrictLeft = ltraversable
 
 instance readableSignal :: Exports.Readable Signal Effect where
   read (Signal s) = s.read
