860a8fe36edcfa5c5004d152fa92f9dfc769b000
Robert Porter <robert.porter@rakuten.com>
2020-05-13 14:05:22 +0900

    More Signal updates

--- src/Wire/Signal.purs
+++ src/Wire/Signal.purs
@@ -3,7 +3,7 @@ module Wire.Signal where
 import Prelude
 import Effect (Effect)
 import Effect.Ref as Ref
-import Wire.Class (class EventSource, source)
+import Wire.Class (class EventSink, class EventSource, source)
 import Wire.Event (Event, Subscriber)
 import Wire.Event as Event
 
@@ -11,14 +11,19 @@ newtype Signal a
   = Signal
   { event :: Event a
   , read :: Effect a
+  , push :: a -> Effect Unit
+  , kill :: Effect Unit
   }
 
-fromSource :: forall source a. EventSource source a => a -> source -> Effect (Signal a)
-fromSource init from = do
+signal :: forall source a. EventSource source a => a -> source -> Effect (Signal a)
+signal init from = do
   value <- Ref.new init
-  shared <- Event.share (source from)
-  _ <- Event.subscribe shared.event (flip Ref.write value)
-  pure $ Signal { event: shared.event, read: Ref.read value }
+  { event, push } <- Event.create
+  cancel <-
+    Event.subscribe (source from) \a -> do
+      Ref.write a value
+      push a
+  pure $ Signal { event: event, read: Ref.read value, push, kill: cancel }
 
 read :: forall a. Signal a -> Effect a
 read (Signal s) = s.read
@@ -28,4 +33,14 @@ subscribe (Signal s) k = do
   _ <- s.read >>= k
   Event.subscribe s.event k
 
-derive instance functorSignal :: Functor Signal
+write :: forall a. a -> Signal a -> Effect Unit
+write a (Signal s) = s.push a
+
+kill :: forall a. Signal a -> Effect Unit
+kill (Signal s) = s.kill
+
+instance eventSourceSignal :: EventSource (Signal a) a where
+  source (Signal s) = s.event
+
+instance eventSinkSignal :: EventSink (Signal a) a where
+  sink (Signal s) from = Event.subscribe (source from) s.push
