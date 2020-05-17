5dc4d0dcc8f140032090561e9c84905cf665f527
Robert Porter <robert.porter@rakuten.com>
2020-05-17 18:13:07 +0900

    Added a constructor for observables, a specialisation of `Event (Either a b)`

---
+++ src/Wire/Observable.purs
@@ -0,0 +1,33 @@
+module Wire.Observable where
+
+import Prelude
+import Data.Either (Either(..))
+import Data.Maybe (Maybe(..), isNothing)
+import Effect (Effect)
+import Effect.Ref as Ref
+import Wire.Event (Event)
+import Wire.Event as Event
+
+type Observable a b
+  = Event (Either a b)
+
+create :: forall a b. Effect { done :: b -> Effect Unit, event :: Observable a b, next :: a -> Effect Unit }
+create = do
+  inner <- Event.create
+  result <- Ref.new Nothing
+  let
+    next a =
+      whenM (isNothing <$> Ref.read result) do
+        inner.push (Left a)
+
+    done b = do
+      Ref.write (Just b) result
+      inner.push (Right b)
+
+    event =
+      Event.makeEvent \emit -> do
+        Ref.read result
+          >>= case _ of
+              Just b -> emit (Right b) *> mempty
+              Nothing -> Event.subscribe inner.event emit
+  pure { event, next, done }
