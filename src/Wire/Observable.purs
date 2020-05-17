faab366d8aa22509968183df053aeaa84535e54c
Robert Porter <robert.porter@rakuten.com>
2020-05-17 18:24:38 +0900

    Un-newtyped Observable

--- src/Wire/Observable.purs
+++ src/Wire/Observable.purs
@@ -5,12 +5,11 @@ import Data.Either (Either(..))
 import Data.Maybe (Maybe(..), isNothing)
 import Effect (Effect)
 import Effect.Ref as Ref
-import Wire.Event (Event, Subscribe)
+import Wire.Event (Event)
 import Wire.Event as Event
-import Wire.Event.Class (class EventSource)
 
-newtype Observable a b
-  = Observable (Event (Either a b))
+type Observable a b
+  = Event (Either a b)
 
 create :: forall a b. Effect { done :: b -> Effect Unit, observable :: Observable a b, next :: a -> Effect Unit }
 create = do
@@ -25,16 +24,10 @@ create = do
       Ref.write (Just b) result
       inner.push (Right b)
 
-    event =
+    observable =
       Event.makeEvent \emit -> do
         Ref.read result
           >>= case _ of
               Just b -> emit (Right b) *> mempty
               Nothing -> Event.subscribe inner.event emit
-  pure { observable: Observable event, next, done }
-
-subscribe :: forall b a. Observable a b -> Subscribe (Either a b)
-subscribe (Observable event) = Event.subscribe event
-
-instance eventSourceObservable :: EventSource (Observable a b) (Either a b) where
-  source (Observable event) = event
+  pure { observable, next, done }
