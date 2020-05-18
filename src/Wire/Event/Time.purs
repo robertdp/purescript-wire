d2b2570e63b1e1ec31fb4ad07f700f51cf1b02cd
Robert Porter <robert.porter@rakuten.com>
2020-05-19 04:09:25 +0900

    Aff as a qualified import

--- src/Wire/Event/Time.purs
+++ src/Wire/Event/Time.purs
@@ -2,7 +2,7 @@ module Wire.Event.Time where
 
 import Prelude
 import Control.Alt ((<|>))
-import Effect.Aff (launchAff_)
+import Effect.Aff as Aff
 import Effect.Class (liftEffect)
 import Effect.Ref as Ref
 import Effect.Timer as Timer
@@ -16,7 +16,7 @@ delay ms event =
     cancel <-
       Event.subscribe event \a ->
         liftEffect do
-          _ <- Timer.setTimeout ms do unlessM (Ref.read canceled) do launchAff_ do emit a
+          _ <- Timer.setTimeout ms do unlessM (Ref.read canceled) do Aff.launchAff_ do emit a
           pure unit
     pure do
       liftEffect do Ref.write true canceled
@@ -26,7 +26,7 @@ interval :: Int -> Event Unit
 interval ms =
   Event.makeEvent \emit ->
     liftEffect do
-      intervalId <- Timer.setInterval ms do launchAff_ do emit unit
+      intervalId <- Timer.setInterval ms do Aff.launchAff_ do emit unit
       pure do liftEffect do Timer.clearInterval intervalId
 
 timer :: Int -> Int -> Event Unit
