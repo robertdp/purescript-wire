6cf131161c3b2bf89f098ced00a825525fb2d626
Robert Porter <robert.porter@rakuten.com>
2020-05-19 00:02:29 +0900

    Signal functions now use Effect

--- src/Wire/Signal.purs
+++ src/Wire/Signal.purs
@@ -1,33 +1,33 @@
 module Wire.Signal where
 
 import Prelude
-import Effect.Aff (Aff)
-import Effect.Class (class MonadEffect, liftEffect)
+import Effect (Effect)
+import Effect.Aff (launchAff_)
+import Effect.Class (liftEffect)
 import Effect.Ref as Ref
 import Wire.Event (Event)
 import Wire.Event as Event
 
-type Signal m a
+type Signal a
   = { event :: Event a
-    , modify :: (a -> a) -> Aff Unit
-    , read :: m a
-    , write :: a -> Aff Unit
+    , modify :: (a -> a) -> Effect Unit
+    , read :: Effect a
+    , write :: a -> Effect Unit
     }
 
-create :: forall m a. MonadEffect m => a -> m (Signal m a)
-create init =
-  liftEffect do
-    value <- Ref.new init
-    inner <- Event.create
-    let
-      read = liftEffect do Ref.read value
+create :: forall a. a -> Effect (Signal a)
+create init = do
+  value <- Ref.new init
+  inner <- Event.create
+  let
+    read = Ref.read value
 
-      write a = modify (const a)
+    write a = modify (const a)
 
-      modify f = (liftEffect do Ref.modify f value) >>= inner.push
+    modify f = Ref.modify f value >>= inner.push >>> launchAff_
 
-      event =
-        Event.makeEvent \emit -> do
-          (liftEffect do Ref.read value) >>= emit
-          Event.subscribe inner.event emit
-    pure { event, read, write, modify }
+    event =
+      Event.makeEvent \emit -> do
+        liftEffect (Ref.read value) >>= emit
+        Event.subscribe inner.event emit
+  pure { event, read, write, modify }
