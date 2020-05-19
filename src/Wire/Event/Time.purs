13ff217fcbdaf3b6651d2ff8121ec4b366c14f39
Robert Porter <robert.porter@rakuten.com>
2020-05-19 17:47:17 +0900

    Use Event.Queue for Time.delay

--- src/Wire/Event/Time.purs
+++ src/Wire/Event/Time.purs
@@ -4,32 +4,26 @@ import Prelude
 import Control.Alt ((<|>))
 import Control.Monad.Rec.Class (forever)
 import Data.Time.Duration (class Duration, fromDuration)
-import Effect.AVar as AVar
 import Effect.Aff as Aff
-import Effect.Aff.AVar as AffVar
 import Effect.Class (liftEffect)
 import Wire.Event (Event)
 import Wire.Event as Event
+import Wire.Event.Queue as Queue
 
 delay :: forall offset a. Duration offset => offset -> Event a -> Event a
 delay offset event = do
   let
     ms = fromDuration offset
   Event.makeEvent \emit -> do
-    queue <- AVar.empty
-    consumer <-
-      (Aff.launchAff <<< forever) do
-        a <- AffVar.take queue
-        liftEffect do emit a
+    queue <- Queue.create emit
     cancel <-
       Event.subscribe event \a ->
         Aff.launchAff_ do
           Aff.delay ms
-          AffVar.put a queue
+          liftEffect do queue.push a
     pure do
       cancel
-      Aff.launchAff_ do Aff.killFiber (Aff.error "cancelled") consumer
-      AVar.kill (Aff.error "cancelled") queue
+      queue.kill
 
 interval :: forall spacing. Duration spacing => spacing -> Event Unit
 interval spacing = do
@@ -41,7 +35,7 @@ interval spacing = do
         Aff.delay ms
         liftEffect do emit unit
     pure do
-      Aff.launchAff_ do Aff.killFiber (Aff.error "cancelled") fiber
+      Aff.launchAff_ do Aff.killFiber (Aff.error "cancelling") fiber
 
 timer :: forall offset spacing. Duration offset => Duration spacing => offset -> spacing -> Event Unit
 timer offset spacing = delay offset do pure unit <|> interval spacing
