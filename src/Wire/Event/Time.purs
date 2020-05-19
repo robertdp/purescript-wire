8a20138885b17b3266fc43547eafac5cabc36df9
Robert Porter <robert.porter@rakuten.com>
2020-05-19 11:35:06 +0900

    Wewrote time helpers

--- src/Wire/Event/Time.purs
+++ src/Wire/Event/Time.purs
@@ -2,32 +2,31 @@ module Wire.Event.Time where
 
 import Prelude
 import Control.Alt ((<|>))
+import Control.Monad.Rec.Class (forever)
+import Effect.Aff (Milliseconds)
 import Effect.Aff as Aff
-import Effect.Class (liftEffect)
-import Effect.Ref as Ref
-import Effect.Timer as Timer
 import Wire.Event (Event)
 import Wire.Event as Event
 
-delay :: forall a. Int -> Event a -> Event a
+delay :: forall a. Milliseconds -> Event a -> Event a
 delay ms event =
   Event.makeEvent \emit -> do
-    canceled <- liftEffect do Ref.new false
     cancel <-
-      Event.subscribe event \a ->
-        liftEffect do
-          _ <- Timer.setTimeout ms do unlessM (Ref.read canceled) do Aff.launchAff_ do emit a
-          pure unit
-    pure do
-      liftEffect do Ref.write true canceled
-      cancel
+      Event.subscribe event \a -> do
+        Aff.delay ms
+        emit a
+    pure cancel
 
-interval :: Int -> Event Unit
+interval :: Milliseconds -> Event Unit
 interval ms =
-  Event.makeEvent \emit ->
-    liftEffect do
-      intervalId <- Timer.setInterval ms do Aff.launchAff_ do emit unit
-      pure do liftEffect do Timer.clearInterval intervalId
+  Event.makeEvent \emit -> do
+    fiber <-
+      Aff.forkAff do
+        forever do
+          Aff.delay ms
+          emit unit
+    pure do
+      Aff.killFiber (Aff.error "cancelling") fiber
 
-timer :: Int -> Int -> Event Unit
+timer :: Milliseconds -> Milliseconds -> Event Unit
 timer after ms = delay after do pure unit <|> interval ms
