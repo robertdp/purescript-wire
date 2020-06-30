ebb92236a7650b8fe1e073c2cdb7c17c597e37d6
Robert Porter <robert.porter@rakuten.com>
2020-06-30 14:25:21 +0900

    Is this magic?

--- src/Wire/Store/Selector.purs
+++ src/Wire/Store/Selector.purs
@@ -1,1 +1,26 @@
 module Wire.Store.Selector where
+
+import Prelude
+import Control.Monad.Free.Trans (FreeT, freeT)
+import Control.Monad.Maybe.Trans (MaybeT(..), lift)
+import Data.Either (Either(..))
+import Data.Symbol (class IsSymbol)
+import Effect (Effect)
+import Prim.Row (class Cons)
+import Wire.Signal (Signal)
+import Wire.Store (Store)
+import Wire.Store as Store
+import Wire.Store.Atom (Atom)
+
+data Selector key (atoms :: # Type) a
+  = Selector' (FreeT (SelectorF atoms) (MaybeT Signal) a) (a -> Effect Unit)
+
+data SelectorF (atoms :: # Type) next
+  = Select (Store atoms -> next)
+
+derive instance functorSelectorF :: Functor (SelectorF atoms)
+
+select :: forall key r atoms value. IsSymbol key => Cons key value r atoms => Atom key value -> FreeT (SelectorF atoms) (MaybeT Signal) value
+select atom =
+  freeT \_ ->
+    pure $ Right $ Select \store -> lift $ MaybeT (Store.lookup atom store).signal
